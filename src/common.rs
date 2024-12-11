81c12a8f0efaddab9eec6c081125cab4d2444927
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-12-11 16:34:25 +0800

    Fix client connection failure or timeout

--- src/common.rs
+++ src/common.rs
@@ -1232,6 +1232,8 @@ pub fn check_process(arg: &str, mut same_uid: bool) -> bool {
 }
 
 pub async fn secure_tcp(conn: &mut FramedStream, key: &str) -> ResultType<()> {
+    // Fix client connection failure or timeout
+    return Ok(());
     let rs_pk = get_rs_pk(key);
     let Some(rs_pk) = rs_pk else {
         bail!("Handshake failed: invalid public key from rendezvous server");
