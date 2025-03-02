add4288bbf4194b7cd4321c7d56ec34c80ee80a9
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-03-02 12:52:32 +0800

    update

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -1,4 +1,4 @@
-package com.carriez.flutter_hbb
+package com.fcurrk.rdplus
 
 import ffi.FFI
 
--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -5,7 +5,7 @@ package ffi
 import android.content.Context
 import java.nio.ByteBuffer
 
-import com.carriez.flutter_hbb.RdClipboardManager
+import com.fcurrk.rdplus.RdClipboardManager
 
 object FFI {
     init {
