f479dd14a9135332ed739f74534db7b1e0c5f35c
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-08-12 12:51:48 +0800

    Update 1.94.1

--- res/rpm-flutter-suse.spec
+++ res/rpm-flutter-suse.spec
@@ -1,5 +1,5 @@
 Name:       rustdesk
-Version:    1.94.0
+Version:    1.94.1
 Release:    0
 Summary:    RPM package
 License:    GPL-3.0
--- src/core_main.rs
+++ src/core_main.rs
@@ -271,6 +271,28 @@ pub fn core_main() -> Option<Vec<String>> {
                     crate::virtual_display_manager::amyuni_idd::uninstall_driver()
                 );
                 return None;
+            } else if args[0] == "--install-remote-printer" {
+                #[cfg(windows)]
+                if crate::platform::is_win_10_or_greater() {
+                    match remote_printer::install_update_printer(&crate::get_app_name()) {
+                        Ok(_) => {
+                            log::info!("Remote printer installed/updated successfully");
+                        }
+                        Err(e) => {
+                            log::error!("Failed to install/update the remote printer: {}", e);
+                        }
+                    }
+                } else {
+                    log::error!("Win10 or greater required!");
+                }
+                return None;
+            } else if args[0] == "--uninstall-remote-printer" {
+                #[cfg(windows)]
+                if crate::platform::is_win_10_or_greater() {
+                    remote_printer::uninstall_printer(&crate::get_app_name());
+                    log::info!("Remote printer uninstalled");
+                }
+                return None;
             }
         }
         #[cfg(target_os = "macos")]
@@ -342,7 +364,7 @@ pub fn core_main() -> Option<Vec<String>> {
                 hbb_common::allow_err!(crate::run_me(vec!["--tray"]));
             }
             #[cfg(windows)]
-            crate::privacy_mode::restore_reg_connectivity(true);
+            crate::privacy_mode::restore_reg_connectivity(true, false);
             #[cfg(any(target_os = "linux", target_os = "windows"))]
             {
                 crate::start_server(true, false);
@@ -465,7 +487,9 @@ pub fn core_main() -> Option<Vec<String>> {
             }
             return None;
         } else if args[0] == "--assign" {
-            if crate::platform::is_installed() && is_root() {
+            if config::Config::no_register_device() {
+                println!("Cannot assign an unregistrable device!");
+            } else if crate::platform::is_installed() && is_root() {
                 let max = args.len() - 1;
                 let pos = args.iter().position(|x| x == "--token").unwrap_or(max);
                 if pos < max {
@@ -501,6 +525,14 @@ pub fn core_main() -> Option<Vec<String>> {
                     if pos < max {
                         address_book_tag = Some(args[pos + 1].to_owned());
                     }
+                    let mut address_book_alias = None;
+                    let pos = args
+                        .iter()
+                        .position(|x| x == "--address_book_alias")
+                        .unwrap_or(max);
+                    if pos < max {
+                        address_book_alias = Some(args[pos + 1].to_owned());
+                    }
                     let mut device_group_name = None;
                     let pos = args
                         .iter()
@@ -533,6 +565,9 @@ pub fn core_main() -> Option<Vec<String>> {
                             body["address_book_name"] = serde_json::json!(name);
                             if let Some(name) = address_book_tag {
                                 body["address_book_tag"] = serde_json::json!(name);
+                            }
+                            if let Some(name) = address_book_alias {
+                                body["address_book_alias"] = serde_json::json!(name);
                             }
                         }
                         if let Some(name) = device_group_name {
